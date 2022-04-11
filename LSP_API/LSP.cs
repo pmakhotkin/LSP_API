2dd1b7a03e19fef2dd1a34743203228ffa3b15c2
Pavel Makhotkin <mahotkin.pavel@yandex.ru>
2022-04-11 14:56:42 +0300

    Добавьте файлы проекта.

---
+++ LSP_API/LSP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP_API
+{
+   public abstract class LSP
+    {
+        public abstract string url_server { get; set; }
+        public abstract string login { get; set; }
+        public abstract string password { get; set; }
+        public abstract string SendRequest(string reguest);
+        // отправка запроса
+        // на вход запрос
+        // возвращает string responce
+        
+        public abstract string CreateRequestNewOrders(List<Order> orders);
+        // возвращает готовый запрос для создания заказа у перевозчика.
+        
+    }
+}
